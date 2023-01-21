bbd99cd99e8d47e6b31213bc76e47ed73784de5b
Kiril <kaloyanov.kiril@gmail.com>
2023-01-21 17:42:25 +0200

    Animate the menu hamburger icon to a cross

--- responsive/03interior-consultant-master/index.js
+++ responsive/03interior-consultant-master/index.js
@@ -3,6 +3,13 @@ const toggleButton = document.querySelector('.hamburger');
 
 const menu = document.querySelector('.menu');
 
+const middleBar = document.querySelector('.middle-bar');
+const upperBar = document.querySelector('.upper-bar');
+const lowerBar = document.querySelector('.lower-bar');
+
 toggleButton.addEventListener('click', function(e) {
     menu.classList.toggle('expanded');
+    middleBar.classList.toggle('hidden-middle-bar');
+    upperBar.classList.toggle('rotated-upper-bar');
+    lowerBar.classList.toggle('rotated-lower-bar');
 });
